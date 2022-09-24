54a5af5085427050059d08878673263a2972dd3c
Anurag Gaur <anuraggaur495@gmail.com>
2022-09-25 02:34:46 +0530

    working contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,14 +5,23 @@ pragma solidity ^0.8.17;
 import 'hardhat/console.sol';
 
 contract WavePortal {
+    
     uint256 totalWaves = 0;
+    event NewWave(address indexed from, uint256 timestamp, string message);
+    struct Wave {
+        address waver;
+        uint256 timestamp;
+        string message;
+    }
+    Wave[] waves;
+
     constructor() {
-        console.log('Smart contract works.');
+        console.log('Smart contract second iteration.');
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log('Wave recieved from ', msg.sender);
+        console.log('Wave recieved from %s with message %s', msg.sender, _message);
     }
 
     function getWaveCount() public view returns(uint256) {
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -9,6 +10,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const GOERLI_URL = process.env.STAGE_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+// const PRIVATE_KEY = process.env.PRIVATE_KEY.toString();
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
