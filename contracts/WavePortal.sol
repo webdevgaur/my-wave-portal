e25a03f80ca07575167ed18f02eaa4f9e1ad1d58
Anurag Gaur <anuraggaur495@gmail.com>
2022-10-01 14:51:06 +0530

    random prize + cooldown

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -17,6 +17,8 @@ contract WavePortal {
     }
     Wave[] waves;
 
+    mapping(address => uint256) public lastWavedAt;
+
     constructor() payable {
         console.log('Smart contract third iteration.');
         seed = (block.timestamp + block.difficulty)%100;
@@ -25,21 +27,30 @@ contract WavePortal {
 
 
     function wave(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 5 minutes < block.timestamp,
+            "Waves need to be separated by 5 minutes."
+        );
+        lastWavedAt[msg.sender] = block.timestamp;
         totalWaves += 1;
         console.log('Wave recieved from %s with message %s', msg.sender, _message);
         waves.push(Wave(msg.sender, block.timestamp, _message));
 
-        uint256 prizeAmount = 0.001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            'Unfortunately, we are out of funds! :('
-        );
-        (bool success,) = (msg.sender).call{value: prizeAmount}("");
-        require(success, 'Unexpected failure. Cannot withdraw money from the contract.');
+        seed = (block.timestamp + block.difficulty + seed) % 100;
+        console.log('Randomly generate seed for new wave:', seed);
 
-        emit NewWave(msg.sender, block.timestamp, _message);
+        if(seed < 50) {
+            console.log('Congratulations! %s you have won!', msg.sender);
+            uint256 prizeAmount = 0.001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                'Unfortunately, we are out of funds! :('
+            );
+            (bool success,) = (msg.sender).call{value: prizeAmount}("");
+            require(success, 'Unexpected failure. Cannot withdraw money from the contract.');
+        }    
 
-        
+        emit NewWave(msg.sender, block.timestamp, _message);        
     }
 
     function getAllWaves() public view returns(Wave[] memory) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,34 +1,53 @@
 const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy({
-      value: hre.ethers.utils.parseEther('0.2'),
+      value: hre.ethers.utils.parseEther('0.1'),
     });
     await waveContract.deployed();
     console.log("Contract resides at:", waveContract.address);
 
     let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
     console.log('Contract balance is:', hre.ethers.utils.formatEther(contractBalance));
-  
-    let waveCount;
-    waveCount = await waveContract.getWaveCount();
-    //console.log('Wave count is',waveCount.toNumber());
-  
+    
     let waveAction = await waveContract.wave("Message from the first wave");
     await waveAction.wait(); // Wait for the transaction to be mined
 
-    // Wavecount after first wave
-    waveCount = await waveContract.getWaveCount();
-    console.log('Wave count is',waveCount.toNumber());
-    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
-    console.log('Contract balance after first wave:', hre.ethers.utils.formatEther(contractBalance));
-  
-    const [_, randomPerson] = await hre.ethers.getSigners();
-    waveAction = await waveContract.connect(randomPerson).wave("Second wave's message");
+    waveAction = await waveContract.wave("Message from the second wave");
     await waveAction.wait(); // Wait for the transaction to be mined
 
-    // Wavecount after second wave
-    waveCount = await waveContract.getWaveCount();
-    console.log('Wave count is',waveCount.toNumber());
+    // contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    // console.log('Contract balance after first wave:', hre.ethers.utils.formatEther(contractBalance));
+  
+    // const [_,
+    //   randomPerson1,
+    //   randomPerson2,
+    //   randomPerson3,
+    //   randomPerson4,
+    //   randomPerson5,
+    //   randomPerson6,
+    //   randomPerson7,    
+    //  ] = await hre.ethers.getSigners();
+    // waveAction = await waveContract.connect(randomPerson1).wave("Second wave's message");
+    // await waveAction.wait(); // Wait for the transaction to be mined
+
+    // waveAction = await waveContract.connect(randomPerson2).wave("Third wave's message");
+    // await waveAction.wait(); // Wait for the transaction to be mined
+
+    // waveAction = await waveContract.connect(randomPerson3).wave("Fourth wave's message");
+    // await waveAction.wait(); // Wait for the transaction to be mined
+    
+    // waveAction = await waveContract.connect(randomPerson4).wave("Fifth wave's message");
+    // await waveAction.wait(); // Wait for the transaction to be mined
+    
+    // waveAction = await waveContract.connect(randomPerson5).wave("Sixth wave's message");
+    // await waveAction.wait(); // Wait for the transaction to be mined
+    
+    // waveAction = await waveContract.connect(randomPerson6).wave("Seventh wave's message");
+    // await waveAction.wait(); // Wait for the transaction to be mined
+    
+    // waveAction = await waveContract.connect(randomPerson7).wave("Eighth wave's message");
+    // await waveAction.wait(); // Wait for the transaction to be mined
+
     contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
     console.log('Contract balance after second wave:', hre.ethers.utils.formatEther(contractBalance));
 
